05a058a6a824640718e80d5052245caeb66d8fd6
MontyPan <monty.pan@dontcareabout.us>
2019-07-16 11:12:59 +0800

    補強日期處理能力

--- src/main/java/us/dontcareabout/homePage/client/data/FTL.java
+++ src/main/java/us/dontcareabout/homePage/client/data/FTL.java
@@ -2,6 +2,9 @@ package us.dontcareabout.homePage.client.data;
 
 import java.util.Date;
 
+import com.google.gwt.user.datepicker.client.CalendarUtil;
+import com.sencha.gxt.core.client.util.DateWrapper;
+
 import us.dontcareabout.homePage.client.gf.GoogleSheetEntry;
 
 public final class FTL extends GoogleSheetEntry {
@@ -11,12 +14,17 @@ public final class FTL extends GoogleSheetEntry {
 		return stringField("船艦");
 	}
 
-	public Date getStart() {
-		return dateField("開始時間");
+	public DateWrapper getStart() {
+		return new DateWrapper(dateField("開始時間"));
 	}
 
-	public Date getEnd() {
-		return dateField("結束時間");
+	public DateWrapper getEnd() {
+		Date date = dateField("結束時間");
+		return date == null ? new DateWrapper() : new DateWrapper(date);
+	}
+
+	public int getLength() {
+		return CalendarUtil.getDaysBetween(getStart().asDate(), getEnd().asDate()) + 1;
 	}
 
 	public int getAmount() {
