2f64e1cb85ea462f8a297ae02f349111371e454e
MontyPan <monty.pan@dontcareabout.us>
2019-07-19 23:57:56 +0800

    修正：跨年份月份 FTL 的顯示問題

--- src/main/java/us/dontcareabout/homePage/client/layer/CalendarLayer.java
+++ src/main/java/us/dontcareabout/homePage/client/layer/CalendarLayer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.sencha.gxt.chart.client.draw.RGB;
+import com.sencha.gxt.core.client.util.DateWrapper;
 
 import us.dontcareabout.gxt.client.draw.LRectangleSprite;
 import us.dontcareabout.gxt.client.draw.LayerSprite;
@@ -11,6 +12,7 @@ import us.dontcareabout.homePage.client.data.FTL;
 
 public class CalendarLayer extends LayerSprite {
 	private HashMap<Integer, MonthLayer> layerMap = new HashMap<>();
+	private int year;
 
 	public CalendarLayer() {
 		for (int i = 0; i < 12; i++) {
@@ -20,13 +22,27 @@ public class CalendarLayer extends LayerSprite {
 		}
 	}
 
-	public void refresh(ArrayList<FTL> data) {
+	/**
+	 * 這裡只考慮 {@link FTL} 起訖年份相差一年以內的狀況。
+	 */
+	public void refresh(int year, ArrayList<FTL> data) {
+		this.year = year;
+
 		for (MonthLayer ml : layerMap.values()) {
 			ml.clean();
 		}
 
 		for (FTL ftl : data) {
-			layerMap.get(ftl.getStart().getMonth()).addRecord(ftl);
+			DateWrapper start = ftl.getStart();
+			DateWrapper end = ftl.getEnd();
+			//起始日期年份小於指定年份，就等同於從年初開始（1 月）
+			int startMonth = start.getFullYear() < year ? 0 : start.getMonth();
+			//終止日期年份大於指定年份，就等同於到年底（12 月）
+			int endMonth = end.getFullYear() > year ? 11 : end.getMonth();
+
+			for (int i = startMonth; i <= endMonth; i++) {
+				layerMap.get(i).addRecord(ftl);
+			}
 		}
 
 		redeploy();
@@ -58,12 +74,22 @@ public class CalendarLayer extends LayerSprite {
 			sections.add(sl);
 			add(sl);
 
+			//校正起始 or 結束月份非本月的對應日期
+			if (sl.record.getStart().getMonth() < month) {
+				sl.startDay = 1;
+			}
+
+			if (sl.record.getEnd().getFullYear() > year || sl.record.getEnd().getMonth() > month) {
+				sl.endDay = new DateWrapper(year, month, 1).getLastDateOfMonth().getDate();
+			}
+			////
+
 			//處理開始日期與上一個結束日期重疊的
 			if (sections.size() == 1) { return; }
 
 			SectionLayer last = sections.get(sections.size() - 2);
 
-			if (last.record.getEnd().getDate() == ftl.getStart().getDate()) {
+			if (last.endDay == sl.startDay) {
 				last.up = overlapUp;
 				overlapUp = !overlapUp;
 				sl.up = overlapUp;
@@ -83,8 +109,8 @@ public class CalendarLayer extends LayerSprite {
 			double widthUnit = getWidth() / 31;
 
 			for (SectionLayer sl : sections) {
-				sl.setLX((sl.record.getStart().getDate() - 1) * widthUnit + space);
-				sl.setWidth(sl.record.getLength() * widthUnit - space * 2);
+				sl.setLX((sl.startDay - 1) * widthUnit + space);
+				sl.setWidth((sl.endDay - sl.startDay + 1) * widthUnit - space * 2);
 
 				if (sl.up == null) {
 					sl.setLY(space);
@@ -102,12 +128,18 @@ public class CalendarLayer extends LayerSprite {
 
 	private class SectionLayer extends LRectangleSprite {
 		FTL record;
+		/** 考慮年份與月份的修正起始日 */
+		int startDay;
+		/** 考慮年份與月份的修正結束日 */
+		int endDay;
 
 		/** null 表示全幅顯示，true 表示只佔上半格、false 表示只佔下半格 */
 		Boolean up;
 
 		SectionLayer(FTL ftl) {
 			record = ftl;
+			startDay = ftl.getStart().getDate();
+			endDay = ftl.getEnd().getDate();
 			setFill(RGB.ORANGE);
 			setRadius(4);
 		}
