5cd5e2aba33becb8506cd0310362e1ff11bd3373
MontyPan <monty.pan@dontcareabout.us>
2019-07-28 17:16:16 +0800

    初版 FTL 對戰紀錄畫面，只掛了 CalendarLayer

---
+++ src/main/java/us/dontcareabout/homePage/client/ui/FtlView.java
@@ -0,0 +1,56 @@
+package us.dontcareabout.homePage.client.ui;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import us.dontcareabout.gxt.client.draw.LayerContainer;
+import us.dontcareabout.homePage.client.data.DataCenter;
+import us.dontcareabout.homePage.client.data.FTL;
+import us.dontcareabout.homePage.client.data.FtlReadyEvent;
+import us.dontcareabout.homePage.client.data.FtlReadyEvent.FtlReadyHandler;
+import us.dontcareabout.homePage.client.layer.CalendarLayer;
+
+public class FtlView extends LayerContainer {
+	private CalendarLayer calendar = new CalendarLayer();
+
+	private HashMap<Integer, ArrayList<FTL>> yearMap = new HashMap<>();
+
+	public FtlView() {
+		addLayer(calendar);
+
+		DataCenter.addFtlReady(new FtlReadyHandler() {
+			@Override
+			public void onFtlReady(FtlReadyEvent event) {
+				refresh(event.data);
+			}
+		});
+		DataCenter.wantFTL();
+	}
+
+	@Override
+	protected void onResize(int width, int height) {
+		calendar.resize(width - 100, height);
+		super.onResize(width, height);
+	}
+
+	private void refresh(ArrayList<FTL> data) {
+		yearMap.clear();
+
+		int maxYear = 0;
+		for (FTL ftl : data) {
+			ArrayList<FTL> year = yearMap.get(ftl.getStart().getFullYear());
+
+			if (year == null) {
+				year = new ArrayList<>();
+				yearMap.put(ftl.getStart().getFullYear(), year);
+			}
+
+			if (ftl.getStart().getFullYear() > maxYear) { maxYear = ftl.getStart().getFullYear(); }
+
+			year.add(ftl);
+		}
+
+		calendar.refresh(maxYear, yearMap.get(maxYear));
+		redrawSurface();
+	}
+}
