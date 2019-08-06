1b37ac9e26a01c6f9093846175450ea46a063a2e
MontyPan <monty.pan@dontcareabout.us>
2019-08-06 15:26:40 +0800

    增加：Util

---
+++ src/main/java/us/dontcareabout/homePage/client/Util.java
@@ -0,0 +1,9 @@
+package us.dontcareabout.homePage.client;
+
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.i18n.client.NumberFormat;
+
+public class Util {
+	public static final DateTimeFormat dateFormat = DateTimeFormat.getFormat("yyyy/MM/dd");
+	public static NumberFormat numberFormat = NumberFormat.getFormat("##.#");
+}
