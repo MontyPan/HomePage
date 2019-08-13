62c2d7d4b39c0c79afa805ae24d8fbb2cc74a4fe
MontyPan <monty.pan@dontcareabout.us>
2019-08-13 23:57:20 +0800

    增加：設定 margin 的能力

--- src/main/java/us/dontcareabout/homePage/client/gf/VerticalLayoutLayer.java
+++ src/main/java/us/dontcareabout/homePage/client/gf/VerticalLayoutLayer.java
@@ -3,19 +3,29 @@ package us.dontcareabout.homePage.client.gf;
 import java.util.ArrayList;
 
 import com.google.common.base.Preconditions;
+import com.sencha.gxt.core.client.util.Margins;
 
-import us.dontcareabout.gwt.client.Console;
 import us.dontcareabout.gxt.client.draw.LayerSprite;
 
 public class VerticalLayoutLayer extends LayerSprite {
 	private ArrayList<LayerSprite> children = new ArrayList<>();
 	private ArrayList<Double> weightList = new ArrayList<>();
+	private Margins margins = new Margins();
 
 	public void addChild(LayerSprite child, double weight) {
 		Preconditions.checkArgument(weight > 0);
 		children.add(child);
 		weightList.add(weight);
 		add(child);
+	}
+
+	public void setMargin(int value) {
+		margins = new Margins(value);
+	}
+
+	public void setMargin(Margins margins) {
+		Preconditions.checkArgument(margins != null);
+		this.margins = margins;
 	}
 
 	@Override
@@ -26,17 +36,18 @@ public class VerticalLayoutLayer extends LayerSprite {
 			if (w > 1) { fixed += w; }
 		}
 
-		double remain = getHeight() - fixed;
-		double y = 0;
+		double remain = getHeight() - fixed - margins.getTop() - margins.getBottom();
+		double y = margins.getTop();
+		double x = margins.getLeft();
+		double width = getWidth() - margins.getLeft() - margins.getRight();
 
 		for (int i = 0; i < children.size(); i++) {
 			LayerSprite child = children.get(i);
 			double weight = weightList.get(i);
 			double height = weight > 1 ? weight : remain * weight;
-			Console.log(weight + " => " + height);
-			child.setLX(0);
+			child.setLX(x);
 			child.setLY(y);
-			child.resize(getWidth(), height);
+			child.resize(width, height);
 			y += height;
 		}
 	}
