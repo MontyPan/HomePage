94244fee3f71d526e8d461497dc7084bbe27d129
MontyPan <monty.pan@dontcareabout.us>
2019-12-28 23:37:30 +0800

    增加：dialog 相關 method

--- src/main/java/us/dontcareabout/homePage/client/Util.java
+++ src/main/java/us/dontcareabout/homePage/client/Util.java
@@ -2,8 +2,28 @@ package us.dontcareabout.homePage.client;
 
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.i18n.client.NumberFormat;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.Window;
 
 public class Util {
 	public static final DateTimeFormat dateFormat = DateTimeFormat.getFormat("yyyy/MM/dd");
 	public static NumberFormat numberFormat = NumberFormat.getFormat("##.#");
+
+	private final static Window dialog = new Window();
+	static {
+		dialog.setModal(true);
+		dialog.setResizable(false);
+	}
+
+	public static void closeDialog() {
+		dialog.hide();
+	}
+
+	public static void showDialog(Widget widget, int width, int height) {
+		dialog.clear();
+		dialog.add(widget);
+		dialog.show();
+		dialog.setPixelSize(width, height);
+		dialog.center();
+	}
 }
