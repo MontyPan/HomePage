70e5cab816f5a72fba1e99b6c5a43795467393ad
MontyPan <monty.pan@dontcareabout.us>
2021-01-28 23:14:07 +0800

    處理「酒」與「加味」comment 顯示邏輯

--- src/main/java/us/dontcareabout/homePage/client/ui/MakeWineView.java
+++ src/main/java/us/dontcareabout/homePage/client/ui/MakeWineView.java
@@ -29,6 +29,8 @@ public class MakeWineView extends LayerContainer {
 	private SummaryLayer summaryLayer = new SummaryLayer();
 
 	public MakeWineView() {
+		wineLayer.setComment(wineComment(Composition.WINE_MAX, null));
+
 		double unit = 1.0 / (Ingredient.values().length + 3);
 
 		root.addChild(wineLayer, unit * 6);
@@ -61,22 +63,69 @@ public class MakeWineView extends LayerContainer {
 
 		int wineRemainder = composition.getWineRemainder();
 
+		if (event.ingredient.type == Type.other) {
+			otherLayer.setComment(otherComment(event.amount));
+		}
+
 		if(event.ingredient.type == Type.wine) {
 			wineLayer.setRemainder(wineRemainder);
 			wineLayer.setComment(wineComment(wineRemainder, composition.getWineComposition()));
 		}
 
-		summaryLayer.setEnable(wineRemainder == 0);
+		summaryLayer.setEnable(wineRemainder == 0 && composition.getBlackBean() != 0);
 
 		if (wineRemainder == 0) {
 			summaryLayer.setTotal(composition.getTotal());
 		}
 	}
 
+	private String otherComment(int amount) {
+		switch(amount) {
+		case 0: return "";
+		case 1: return "有點甜～ 有點補～";
+		case 2: return "這加得好像有點多...";
+		case 3: return "你一定是台南人 [指]";
+		default: return "這是黑豆酒不是桂圓酒啊，大佬？";
+		}
+	}
+
 	private static String wineComment(int remainder, HashMap<Ingredient, Integer> map) {
 		if (remainder != 0) { return "還需要加 " + remainder + " 瓶酒"; }
 
-		return "";//FIXME
+		if (map.size() == 1) {
+			if (map.containsKey(Ingredient.rice)) {
+				return "我阿罵的麻油雞都比這個濃...";
+			}
+			if (map.containsKey(Ingredient.ricePure)) {
+				return "這批很純！";
+			}
+			if (map.containsKey(Ingredient.riceHead)) {
+				return "內行的都加米酒頭 (y)";
+			}
+			if (map.containsKey(Ingredient.sorghum38)) {
+				return "有種三八的味道 [捏鼻]";
+			}
+			if (map.containsKey(Ingredient.sorghum58)) {
+				return "黑豆酒界的生命之水 [抖]";
+			}
+		}
+
+		if (map.size() == 2) {
+			Integer pure = map.get(Ingredient.ricePure);
+			Integer head = map.get(Ingredient.riceHead);
+
+			if (pure != null && head != null && pure == 5 && head == 1) {
+				return "\\囧/ 太座指定組合 \\囧/";
+			}
+
+			return "你濃我濃（？）";
+		}
+
+		if (map.size() == 3) { return "三分天下"; }
+		if (map.size() == 4) { return "湊在一起打麻將"; }
+		if (map.size() == 5) { return "混搭大師"; }
+
+		return "糟糕，有駭客！？";	//不可能發生... 除非有 bug...
 	}
 
 	private class BR extends LayerSprite {
