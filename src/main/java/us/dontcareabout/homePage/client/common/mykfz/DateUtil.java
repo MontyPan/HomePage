5c6b0c9530698b92bc17597c5b603b211417dd72
MontyPan <monty.pan@dontcareabout.us>
2021-03-20 16:15:22 +0800

    增加：DateUtil

---
+++ src/main/java/us/dontcareabout/homePage/client/common/mykfz/DateUtil.java
@@ -0,0 +1,28 @@
+package us.dontcareabout.homePage.client.common.mykfz;
+
+import java.util.Date;
+
+import com.google.gwt.user.datepicker.client.CalendarUtil;
+import com.sencha.gxt.core.client.util.DateWrapper;
+
+public class DateUtil {
+	public static final DateWrapper start = new DateWrapper(2021, 2, 17);
+
+	/**
+	 * @return 指定的日期是第幾季。（起算日期為 {@link #start}）
+	 */
+	public static int session(Date d) {
+		DateWrapper date = new DateWrapper(d);
+		return (int) Math.floor(
+			CalendarUtil.getDaysBetween(start.asDate(), date.asDate()) / 14.0
+		) + 1;
+	}
+
+	/**
+	 * @return 目前是第幾季。
+	 * @see #session(Date)
+	 */
+	public static int nowSession() {
+		return session(new Date());
+	}
+}
