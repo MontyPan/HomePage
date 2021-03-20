3ff0a738afc554041483c6b8a703317c28274494
MontyPan <monty.pan@dontcareabout.us>
2021-03-21 00:38:38 +0800

    增加：處理 SelectPlayerChangeEvent

--- src/main/java/us/dontcareabout/homePage/client/ui/MykfzView.java
+++ src/main/java/us/dontcareabout/homePage/client/ui/MykfzView.java
@@ -1,6 +1,9 @@
 package us.dontcareabout.homePage.client.ui;
 
+import java.util.List;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.event.shared.SimpleEventBus;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -11,8 +14,11 @@ import us.dontcareabout.homePage.client.common.mykfz.DateUtil;
 import us.dontcareabout.homePage.client.component.mykfz.LeaderBoardChart;
 import us.dontcareabout.homePage.client.component.mykfz.LeaderBoardGrid;
 import us.dontcareabout.homePage.client.data.DataCenter;
+import us.dontcareabout.homePage.client.data.Mykfz;
 import us.dontcareabout.homePage.client.data.MykfzReadyEvent;
 import us.dontcareabout.homePage.client.data.MykfzReadyEvent.MykfzReadyHandler;
+import us.dontcareabout.homePage.client.ui.event.mykfz.SelectPlayerChangeEvent;
+import us.dontcareabout.homePage.client.ui.event.mykfz.SelectPlayerChangeEvent.SelectPlayerChangeHandler;
 
 public class MykfzView extends Composite {
 	private static MykfzViewUiBinder uiBinder = GWT.create(MykfzViewUiBinder.class);
@@ -23,6 +29,12 @@ public class MykfzView extends Composite {
 
 	public MykfzView() {
 		initWidget(uiBinder.createAndBindUi(this));
+		addSelectPlayerChange(new SelectPlayerChangeHandler() {
+			@Override
+			public void onSelectPlayerChange(SelectPlayerChangeEvent event) {
+				leaderBoardChart.selectPlayer(event.data);
+			}
+		});
 		DataCenter.addMykfzReady(new MykfzReadyHandler() {
 			@Override
 			public void onMykfzReady(MykfzReadyEvent event) {
@@ -33,7 +45,13 @@ public class MykfzView extends Composite {
 		DataCenter.wantMykfz(DateUtil.nowSession());
 	}
 
+	public static HandlerRegistration addSelectPlayerChange(SelectPlayerChangeHandler handler) {
+		return eventBus.addHandler(SelectPlayerChangeEvent.TYPE, handler);
+	}
+
+	public static void selectPlayerChange(List<Mykfz> selectPlayer) {
+		eventBus.fireEvent(new SelectPlayerChangeEvent(selectPlayer));
+	}
+
 	interface MykfzViewUiBinder extends UiBinder<Widget, MykfzView> {}
-
-
 }
