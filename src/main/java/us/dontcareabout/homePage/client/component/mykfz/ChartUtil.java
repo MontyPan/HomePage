2585db56629fc16bc822804aad7fd85f896296b9
MontyPan <monty.pan@dontcareabout.us>
2021-03-20 17:46:45 +0800

    增加：levelUnit()、mantissaWeight()

--- src/main/java/us/dontcareabout/homePage/client/component/mykfz/ChartUtil.java
+++ src/main/java/us/dontcareabout/homePage/client/component/mykfz/ChartUtil.java
@@ -17,11 +17,19 @@ class ChartUtil {
 
 		int result = LV_WEIGHT_UNIT;
 
-		for (int i = 1; i < level - LV_MIN + 1; i++) {
-			result += i * LV_WEIGHT_UNIT * 2;
+		for (int i = LV_MIN; i <= level; i++) {
+			result += levelUnit(i) * LV_WEIGHT_UNIT;
 		}
 
 		return result;
+	}
+
+	static int mantissaWeight(int level, double mantissa) {
+		return ((int) Math.ceil(mantissa / 1000.0 * levelUnit(level))) * LV_WEIGHT_UNIT;
+	}
+
+	static int levelUnit(int level) {
+		return (level - LV_MIN ) * 3;
 	}
 
 	static String inverseLevel(Number weight) {
