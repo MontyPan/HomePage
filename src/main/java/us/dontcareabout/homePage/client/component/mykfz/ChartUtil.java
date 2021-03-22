7905f0977680b296c71af5e3ae1dd41263bc333a
MontyPan <monty.pan@dontcareabout.us>
2021-03-22 17:04:49 +0800

    修正 levelUnit() 與其他 method 的定義矛盾，並簡化 levelWeight() 的計算

--- src/main/java/us/dontcareabout/homePage/client/component/mykfz/ChartUtil.java
+++ src/main/java/us/dontcareabout/homePage/client/component/mykfz/ChartUtil.java
@@ -5,6 +5,7 @@ class ChartUtil {
 	public static final int LV_MIN = 4;
 	public static final int LV_MAX = LEVEL.length;
 	public static final int LV_WEIGHT_UNIT = 5;
+	public static final int LV_UNIT_D = 3;
 
 	static double outputWeight(int level, double mantissa) {
 		if (level < LV_MIN) { return 0; }
@@ -12,24 +13,28 @@ class ChartUtil {
 		return levelWeight(level) + mantissa / 100.0 * (level - LV_MIN + 1);
 	}
 
+	/**
+	 * @return 指定 level 的起點 y 座標
+	 * @see #levelUnit(int)
+	 */
 	static int levelWeight(int level) {
 		if (level < LV_MIN) { return 0; }
 
-		int result = LV_WEIGHT_UNIT;
+		int n = level - LV_MIN + 1;
+		//a0 = 0 的等差級數求和公式簡化，外加一個 0～LV_MIN 的基本高度
+		return (1 + (n * (n - 1)) * LV_UNIT_D / 2) * LV_WEIGHT_UNIT;
+	}
 
-		for (int i = LV_MIN; i <= level; i++) {
-			result += levelUnit(i) * LV_WEIGHT_UNIT;
-		}
-
-		return result;
+	/**
+	 * levelUnit() 為每個 level（到下個 level）的高度。
+	 * levelUnit(n) 為等差數列，方便起見設 a0 為 level 3
+	 */
+	static int levelUnit(int level) {
+		return (level - LV_MIN + 1) * LV_UNIT_D;
 	}
 
 	static int mantissaWeight(int level, double mantissa) {
 		return ((int) Math.ceil(mantissa / 1000.0 * levelUnit(level))) * LV_WEIGHT_UNIT;
-	}
-
-	static int levelUnit(int level) {
-		return (level - LV_MIN ) * 3;
 	}
 
 	static String inverseLevel(Number weight) {
