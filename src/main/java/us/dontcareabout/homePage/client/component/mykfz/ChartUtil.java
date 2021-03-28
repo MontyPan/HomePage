f23cc681df774689da24a4f7c0b5709148cff748
MontyPan <monty.pan@dontcareabout.us>
2021-03-29 01:57:11 +0800

    各 level 高度從原本的等差數列改為等比數列

--- src/main/java/us/dontcareabout/homePage/client/component/mykfz/ChartUtil.java
+++ src/main/java/us/dontcareabout/homePage/client/component/mykfz/ChartUtil.java
@@ -2,10 +2,10 @@ package us.dontcareabout.homePage.client.component.mykfz;
 
 class ChartUtil {
 	public static final String[] LEVEL = {"", "K", "M", "B", "T", "aa", "ab", "ac", "ad"};
-	public static final int LV_MIN = 4;
+	public static final int LV_MIN = 3;
 	public static final int LV_MAX = LEVEL.length;
 	public static final int LV_WEIGHT_UNIT = 5;
-	public static final int LV_UNIT_D = 3;
+	public static final int LV_UNIT_R = 3;
 
 	static double outputWeight(int level, double mantissa) {
 		if (level < LV_MIN) { return 0; }
@@ -18,19 +18,19 @@ class ChartUtil {
 	 * @see #levelUnit(int)
 	 */
 	static int levelWeight(int level) {
-		if (level < LV_MIN) { return 0; }
+		if (level <= LV_MIN) { return 0; }
 
-		int n = level - LV_MIN + 1;
-		//a0 = 0 的等差級數求和公式簡化，外加一個 0～LV_MIN 的基本高度
-		return (1 + (n * (n - 1)) * LV_UNIT_D / 2) * LV_WEIGHT_UNIT;
+		return (int)((Math.pow(LV_UNIT_R, level - LV_MIN) - 1) / (LV_UNIT_R - 1) * LV_WEIGHT_UNIT);
 	}
 
 	/**
 	 * levelUnit() 為每個 level（到下個 level）的高度。
-	 * levelUnit(n) 為等差數列，方便起見設 a0 為 level 3
+	 * levelUnit(n) 為等比數列，a1 為 {@link #LV_MIN}
 	 */
 	static int levelUnit(int level) {
-		return (level - LV_MIN + 1) * LV_UNIT_D;
+		if (level < LV_MIN) { return 0; }
+
+		return (int)(Math.pow(LV_UNIT_R, level - LV_MIN));
 	}
 
 	static int mantissaWeight(int level, double mantissa) {
